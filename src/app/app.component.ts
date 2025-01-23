d3efb040373d09315ee3efb69e887fc3c8357224
irineufreitas <freitaschoro@gmail.com>
2025-01-23 02:48:00 -0300

    cms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header.component';
+import { ContactListComponent } from "./contacts/contact-list/contact-list.component";
+import { ContactsComponent } from './contacts/contacts.component';
+import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
 
 @Component({
   selector: 'cms-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, ContactListComponent, ContactsComponent, ContactDetailComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/contacts/contact-detail/contact-detail.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Contact } from '../contact.model';
+import { CommonModule } from '@angular/common';
+
+
+@Component({
+  selector: 'cms-contact-detail',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './contact-detail.component.html',
+  styleUrl: './contact-detail.component.css'
+})
+export class ContactDetailComponent {
+  contact: Contact = new Contact(
+    '1',
+    'R. Kent Jackson',
+    'jacksonk@byui.edu',
+    '208-496-3771',
+    'assets/images/jacksonk.jpg',
+    null
+  );
+
+}
